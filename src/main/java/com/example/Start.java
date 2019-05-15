66755d0b8db947274d8eeb643b6bdda04503d229
Abhishek Chakraborty <abhishek.y2a@gmail.com>
2019-05-16 00:45:45 +0530

    ---test changes committed---

--- src/main/java/com/example/Start.java
+++ src/main/java/com/example/Start.java
@@ -6,5 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Start {
     public static void main(String[] args) {
         SpringApplication.run(Start.class, args);
+        System.out.println("I am in main");
     }
 }
